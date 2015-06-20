af21985608c3c7d611c7b1a1f984f85d1f3b39df
mattak <mattak.me@gmail.com>
2015-06-20 17:48:40 +0900

    feat(Scripts/DonutsFloorBehaviour): fixed donuts floor

---
+++ Assets/Resources/Scripts/DonutsFloorBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class DonutsFloorBehaviour : MonoBehaviour {
+	public GameObject donutsObject;
+
+	// Use this for initialization
+	void Start () {
+		this.UpdateAsObservable()
+			.Select (_ => donutsObject.transform.position)
+			.Subscribe (pos => {
+				this.transform.position = new Vector3(pos.x, pos.y+0.7f, this.transform.position.z);
+			});
+	}
+}
