dfe887c46c590ee790257d8c592a8381bafd99e4
mattak <mattak.me@gmail.com>
2015-06-20 17:20:32 +0900

    feat(Script/CameraBehavior): track charactor

---
+++ Assets/Resources/Scripts/CameraBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class CameraBehaviour : MonoBehaviour {
+	public GameObject trackObject;
+
+	// Use this for initialization
+	void Start () {
+		IObservable<Vector3> positionObservable = Observable.EveryUpdate()
+			.Select (_ => { return trackObject.transform.position; });
+
+		positionObservable.Subscribe(pos => { 
+			Camera.main.transform.position = new Vector3(pos.x, pos.y, Camera.main.transform.position.z);
+		});
+	}
+}
