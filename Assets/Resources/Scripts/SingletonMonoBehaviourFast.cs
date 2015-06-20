6f3a29f11ee5af79a1e7c08d97b64bd3a5931dd7
mattak <mattak.me@gmail.com>
2015-06-20 19:40:16 +0900

    feat(Scripts/SpeedLimitBehaviour): speed limit setted

---
+++ Assets/Resources/Scripts/SingletonMonoBehaviourFast.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public abstract class SingletonMonoBehaviourFast<T> : MonoBehaviour where T : SingletonMonoBehaviourFast<T> {
+	protected static readonly string[] findTags = {
+		"GameController"
+	};
+
+	protected static T instance;
+	public static T Instance {
+		get {
+			if (instance == null) {
+				Type type = typeof(T);
+				foreach (var tag in findTags) {
+					GameObject[] objs = GameObject.FindGameObjectsWithTag (tag);
+
+					for ( int j = 0; j < objs.Length; j++ ) {
+						instance = (T)objs[j].GetComponent(type);
+
+						if (instance != null) {
+							return instance;
+						}
+					}
+				}
+
+				Debug.LogWarning ( string.Format( string.Format ("{0} is not found", type.Name) ) );
+			}
+
+			return instance;
+		}
+	}
+
+	virtual protected void Awake() {
+		CheckInstance();
+	}
+
+	protected bool CheckInstance() {
+		if (instance == null) {
+			instance = (T)this;
+			return true;
+		}
+		else if (Instance == this) {
+			return this;
+		}
+
+		Destroy (this);
+		return false;
+	}
+}
---
+++ Assets/Resources/Scripts/SpeedLimitBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class SpeedLimitBehaviour : MonoBehaviour {
+	public float maxSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+		this.gameObject.UpdateAsObservable()
+			.Where (_ => rigidbody.velocity.magnitude > maxSpeed)
+			.Subscribe (_ => {
+				rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+			});
+	}
+}
