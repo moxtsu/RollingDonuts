9f877c8b12c2c8e21fb114a3494f2828bcf98a2e
mattak <mattak.me@gmail.com>
2015-06-21 12:32:33 +0900

    feat(Scripts/StartGame): start game scene created

---
+++ Assets/Resources/Scripts/StartGame/StartButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void OnClickStartButton() {
+		Application.LoadLevel("Main");
+	}
+}
