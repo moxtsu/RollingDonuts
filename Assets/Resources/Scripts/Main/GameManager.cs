79aaa246c937825b5ed2dd5b41bc18bb38c27a04
mattak <mattak.me@gmail.com>
2015-06-21 15:13:46 +0900

    Gaugeが動くぞ！！

--- Assets/Resources/Scripts/Main/GameManager.cs
+++ Assets/Resources/Scripts/Main/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using UniRx;
@@ -6,14 +7,25 @@ using UniRx.Triggers;
 
 public class GameManager : MonoBehaviour {
 	public float timeLimit = 10.0f;
+	public Slider slider;
 	
 	// Use this for initialization
 	void Start () {
+		TimeSpan startTime = GetEpoch();
+		
 		this.gameObject.UpdateAsObservable()
 			.Take(1)
 			.Delay(TimeSpan.FromSeconds(timeLimit))
 			.Subscribe(_ => {
 				GameClear();
+			});
+			
+		this.gameObject.UpdateAsObservable()
+			.Select(_ => (GetEpoch() - startTime).TotalSeconds)
+			.Where(passedTime => passedTime < timeLimit)
+			.Select(passedTime => (float)(passedTime / timeLimit))
+			.Subscribe(value => {
+				slider.normalizedValue = value;
 			});
 	}
 	
@@ -23,5 +35,9 @@ public class GameManager : MonoBehaviour {
 	
 	public void GameClear() {
 		Application.LoadLevel ("Clear");
+	}
+	
+	private TimeSpan GetEpoch() {
+		return DateTime.UtcNow - new DateTime(1970, 1, 1);
 	}
 }
