16e1be6cae83a2f1c927c48dc20aef316934f7d8
mattak <mattak.me@gmail.com>
2015-06-21 11:19:25 +0900

    refactor(Sprites/*.meta): add meta files

---
+++ Assets/Resources/Scripts/StartFlagBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class StartFlagBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.OnTriggerEnterAsObservable()
+			.Select (collider => collider.tag == "Character")
+			.Subscribe (_ => {
+					Debug.Log ("TODO: write camera move");
+			});
+	}
+}
