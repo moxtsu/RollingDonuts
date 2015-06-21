37f0e7457b4f2ef164b13cd18e814d152b469cc2
mattak <mattak.me@gmail.com>
2015-06-21 12:00:04 +0900

    feat(Scripts/CharactorController): charactor controller

---
+++ Assets/Resources/Scripts/CharactorController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class CharactorController : MonoBehaviour {
+	private float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+		this.UpdateAsObservable()
+			.Where(_ => Input.GetMouseButton(0))
+			.Select(_ => Input.mousePosition.x)
+			.Select(x => x/Screen.width >= 0.5f)
+			.Subscribe(right => {
+				Debug.Log("right:" + right);
+				rigidbody.velocity = (right)
+					? rigidbody.velocity + new Vector2(2.0f, -0.6f).normalized * speed
+					: rigidbody.velocity + new Vector2(2.0f, -0.6f).normalized * -speed;
+			});
+	}
+}
